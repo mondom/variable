34705d495f407084959216412b781f1ae36d08e8
Monika <codingbymon@gmail.com>
2024-03-20 13:50:09 +0100

    second solution -course

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let root = document.documentElement;
-const spacingInput = document.querySelector('#spacing')
-const blurInput = document.querySelector('#blur')
-const baseInput = document.querySelector('#base')
+// let root = document.documentElement;
+// const spacingInput = document.querySelector('#spacing')
+// const blurInput = document.querySelector('#blur')
+// const baseInput = document.querySelector('#base')
 
-const changeSpacing = () => {
-    root.style.setProperty('--spacing', `${spacingInput.value}px`)
-}
-const changeBlur = () => {
-    root.style.setProperty('--blur', `${blurInput.value}px`)
-}
-const changeColor = () => {
-    root.style.setProperty('--base', `${baseInput.value}`)
+// const changeSpacing = () => {
+//     root.style.setProperty('--spacing', `${spacingInput.value}px`)
+// }
+// const changeBlur = () => {
+//     root.style.setProperty('--blur', `${blurInput.value}px`)
+// }
+// const changeColor = () => {
+//     root.style.setProperty('--base', `${baseInput.value}`)
+// }
+
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate() {
+	const suffix = this.dataset.sizing || ''
+	
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+
 }
 
+inputs.forEach(input => {
+	input.addEventListener('change', handleUpdate)
+})
+
+inputs.forEach(input => {
+	input.addEventListener('mousemove', handleUpdate)
+})
+
+// zmiana wartości zmiennych w css z użyciem metody dataset - notatki oraz this
